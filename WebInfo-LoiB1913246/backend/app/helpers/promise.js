f2574851e586feb20cb600ee825f25f0ba0b5bb8
Nguyễn Chí Lợi <71629314+NCLoi@users.noreply.github.com>
2021-11-28 23:22:40 +0700

    Delete WebInfo-LoiB1913246 directory

--- WebInfo-LoiB1913246/backend/app/helpers/promise.js
+++
@@ -1,7 +0,0 @@
-const handle = (promise) => {
-    return promise
-        .then((data) => [null, data])
-        .catch((error) => [error, undefined]);
-};
-
-module.exports = handle;
--- WebInfo-LoiB1913246/backend/app/middlewares/index.js
+++
@@ -1,7 +0,0 @@
-const { checkDuplicateUsernameOrEmail } = require("./verify-signup");
-const { verifyToken } = require("./auth-jwt");
-
-module.exports = {
-    checkDuplicateUsernameOrEmail,
-    verifyToken,
-};
